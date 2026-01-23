e3b10dbcc31136b1e3b03ede0dec860826771143
Chris <chevalier@chris-freelance.com>
2026-01-23 11:15:17 +0100

    Fixture to create Users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,25 +2,39 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    // Injecte la dépendance UserPasswordHasherInterface pour instancier la classe 'User'
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        // Instancie Faker pour des données fictives
         $faker = Factory::create();
+        // Instancie 20 objets de type 'User'
         for ($i = 0; $i < 20; $i++) {
-            $product = new Product();
-            $product->setDescription($faker->text());
-            $product->setName($faker->name());
-            $product->setPrice($faker->randomNumber());
-            $product->setExpirationDate($faker->dateTime());
-            $manager->persist($product);
+            $user = new User($this->passwordHasher);
+            // Email fictif
+            $user->setEmail($faker->email());
+            // Rôle par défaut
+            $user->setRoles(["ROLE_USER"]);
+            // Mot de passe simple à retenir
+            $user->setPassword("123456");
+            // Ajouter à l'entity Manager
+            $manager->persist($user);
         }
-
+        // Synchronise avec la BDD
         $manager->flush();
     }
 }
